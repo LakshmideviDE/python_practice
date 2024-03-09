258f00c1e5bd207269b61e5de8420cbc8aa11f7b
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-09 18:26:29 +0530

    while loop

---
+++ while.py
@@ -0,0 +1,16 @@
+#while loop
+lst=[12,4,45,67,90]
+i=0
+while i<5:
+    print(lst[i])
+    i=i+1
+#for loop
+lst = [12, 4, 45, 67, 90]
+for i in lst:
+    print(i)
+#range
+print(list(range(2,10)))
+#adding one element
+lst=[23,5,6,8,35]
+lst=[x+1 for x in lst]
+print(lst)
