efc3c6f8d02b9c881ac664b0bcbf5bc461e97d4e
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-09 18:10:57 +0530

    adding keys and values

---
+++ key.py
@@ -0,0 +1,11 @@
+d={1:'c',2:'java',3:'python'}
+print(list(d.keys()))
+for i in d.keys():
+    print(i,d[i])
+
+#values print
+d={26:'spark',12:'java',20:'python'}
+print(list(d.values()))
+for i in d.values():
+    print(i)
+
