d695a30698eee89a40eff1cc40d3fcc614f0a68a
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-09 18:24:25 +0530

    string slicing

---
+++ string.py
@@ -0,0 +1,16 @@
+s1=' '
+print(s1)
+s2=' '
+print(s2)
+s3='python'
+print(s3)
+s4='''c java python '''
+print(s4)
+s5=str(99.0)
+print(s5)
+s6=('practices makes "perfect"')
+print(s6)
+s7="python"
+print(s7[2])
+
+
