dfb1eb251d8c9e820abf45648f3ee03880aaf1e2
lakshmidevig <lakshmidevig870@gmail.com>
2024-03-09 18:00:02 +0530

    adding the elements in a dictionary

---
+++ dict.py
@@ -0,0 +1,15 @@
+d={'two':45,1:92,16:23,65:27,26:88}
+print(d['two'])
+print(26 in d)
+
+#adding the elements dictionary
+d={1:'c',2:'java',3:'python'}
+print(d)
+d[4]='c++'
+print(d)
+d.update({5:'c#',6:'visualBasic'})
+print(d)
+d.update(seven='spark',eight='sql')
+print(d)
+d[2]='python'
+print(d)
